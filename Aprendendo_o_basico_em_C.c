c987aea2c9047d4c0b0f710ebd4bf0b405a9ba43
gghosth <42755106+gghosth@users.noreply.github.com>
2019-04-15 16:43:49 -0300

    Melhorando funções e adicionando sobre variáveis globais

--- Aprendendo_o_basico_em_C.c
+++ Aprendendo_o_basico_em_C.c
@@ -3,9 +3,12 @@
 // Qualquer dúvida entre em contato. links de contato em: http://somnus.dev
 // Anotações básicas e úteis no fim do programa.
 
+
 /*
 
 Ordem:
+
+ (Comece o programa pela linha pontilhada após ver sobre as bibliotecas)
 
 - Usando bibliotecas
 - Começando o programa
@@ -34,7 +37,14 @@ Ordem:
 #include<locale.h>  // Biblioteca para alterar idiomas.
 
 
+int global; // declarando variável global
 
+
+void mostrarGlobal(){
+		
+	printf("\nO valor da variável global na função é: %d\n\n", global);   // mostrando o valor da variável global na função.
+		
+}
 
 
 
@@ -53,8 +63,12 @@ void printMsg(); // O natural seria digitar a função acima do main, mas como é m
 
 
 
+
+// --------COMECE POR AQUI!!!!!!----------------------------------------------------------------------------------------------------------
+
+
+
  //Começando o programa
- 
  
  
 int main(void){ // Chamando a função principal, onde o programa começa. Usamos o "int" e o "void" para evitar problemas ocasionais, porém funciona apenas main(). Devemos abrir e fechar a função com { no começo e } no fim.
@@ -85,7 +99,7 @@ int main(void){ // Chamando a função principal, onde o programa começa. Usamos o
 	
 	scanf("%c",&caractere); // Scaneando do tipo char, %c.
 	
-	printf("\nEscrevi em inteiro: %d.\nEscrevi em caractere: %c.\n",inteiro,caractere); //Escrevendo na tela o valor das variaveis int e char, deve colocar o nome das variaveis fora das aspas.
+	printf("\nEscrevi em inteiro: %d.\nEscrevi em caractere: %c.\n\n",inteiro,caractere); //Escrevendo na tela o valor das variaveis int e char, deve colocar o nome das variaveis fora das aspas.
 	
 	system("pause"); // Pausa o programa até algum botão ser apertado.
 	
@@ -135,23 +149,23 @@ int main(void){ // Chamando a função principal, onde o programa começa. Usamos o
 	switch(usandoSC){ // Para utilizar o switch, chamamos a função "switch(){ }" e vamos usar o nome da variavel nos parenteses (parâmetro). 
 		
 	case 'm':										// caso o usuário tenha digitado m (ou masculino, já que char só vai pegar a primeira letra):
-		printf("\nSeu sexo é masculino\n");  		// Escreve que o sexo é masculino
+		printf("\nSeu sexo é masculino\n\n");  		// Escreve que o sexo é masculino
 		break;										// break quebra o switch e passa para a próxima instrução
 		
 	case 'M':										// Mesma coisa do de cima, mas como a linguagem é case sensitive, temos que fazer maiusculo, vamos aprender como burlar isso depois.
-		printf("\nSeu sexo é masculino\n");
+		printf("\nSeu sexo é masculino\n\n");
 		break;
 		
 	case 'f':
-		printf("\nSeu sexo é feminino\n");
+		printf("\nSeu sexo é feminino\n\n");
 		break;
 		
 	case 'F':
-		printf("\nSeu sexo é feminino\n");
+		printf("\nSeu sexo é feminino\n\n");
 		break;
 		
 	default:																	//default é acionado caso nenhuma das condições serem resolvidas. É o "else" do switch.
-		printf("\nO programa só funciona com sexo masculino e feminino\n");	
+		printf("\nO programa só funciona com sexo masculino e feminino\n\n");	
 			
 		
 	}
@@ -204,7 +218,6 @@ int main(void){ // Chamando a função principal, onde o programa começa. Usamos o
 	system("pause");
 	
 	system("cls");
-	
 
 
 
@@ -228,8 +241,8 @@ int main(void){ // Chamando a função principal, onde o programa começa. Usamos o
 	system("pause");
 	
 	system("cls");
-	
 
+	
 	
 	
 	
@@ -258,8 +271,10 @@ int main(void){ // Chamando a função principal, onde o programa começa. Usamos o
 			
 			
 		}
-
 	
+	system("cls");
+
+
 
 
 
@@ -289,12 +304,20 @@ int main(void){ // Chamando a função principal, onde o programa começa. Usamos o
 										   // Outro exemplo: for(teste=30;teste=0;teste--){ "Conteudo dentro do laço" }, no caso o laço vai se repetir até "teste" descer de 30 para 0.
 											
 									
-	printf("\nVetor posição %d: %d", contador+1,vetorDW[contador]);
+	printf("\nVetor posição %d: %d\n", contador+1,vetorDW[contador]);
 
 	}
 	
+	system("pause");
+	
 	system("cls");
 	
+	
+	
+	
+	
+	
+		
 	//Sub-Rotinas: Procedimentos
 	
 	printMsg(); // Um procedimento, é uma "função sem retorno". Utilizamos para fazer algo no programa que não precise de retorno, como printar uma mensagem. 
@@ -304,21 +327,61 @@ int main(void){ // Chamando a função principal, onde o programa começa. Usamos o
 
 	system("cls");
 	
+	
+	
+	
+	
+	
+	
 	//Sub-Rotinas: Funções
 	
-	int func = 5; // Vamos fazer uma função com retorno do dobro do número, então declaramos "func" como um número qualquer.
+	int func; // Vamos fazer uma função com retorno do dobro do número, então declaramos "func" como inteiro.
 	
 	int funcRes;  // Declaramos um int para receber o retorno da função.
+	
+	printf("Digite um número para saber seu dobro: ");
+	
+	scanf("%d",&func);
 	
 	funcRes = dobroNum(func); // Para passar um valor pra função, colocamos o valor dentro dos parenteses. Com isso, estamos passando um parâmetro para a função. Estamos usando passagem por valor.
 							  // A passagem por valor faz uma cópia do que existe dentro da variável em um novo espaço de memória. Veremos apenas esse meio por enquanto.
 							  // Desta vez, a função foi feita toda antes do main, veja lá em cima.
+							  // funcRes recebe o retorno da função dobroNum() com o parâmetro func.
 	
-	printf("O dobro do número é: %d\n",funcRes); // Verificando se tudo funcionou.
+	printf("\n\nO dobro do número é: %d\n\n",funcRes); // Verificando se tudo funcionou.
 		
 	system("pause");
 	
 	system("cls");
+	
+	
+	
+	
+	
+	
+	// Variáveis globais / locais
+	
+	// Instaciamos uma variável int antes do "main", veja em cima.
+	// Ao fazer isso, instanciamos uma variável global. Variáveis globais são variáveis fora de qualquer função.
+	// Ao usar uma variável global, o valor dela serve para todo o programa, em todas as funções a variável é a mesma.
+	// Ao criar uma variável dentro de uma função, seja o main ou não, ela só existe dentro do escopo dessa função, por isso passamos variáveis por parâmetros.]
+	// Variáveis globais podem ser úteis de vez em quando, mas não são uma prática tão boa. Para utilizar isso de outra maneira melhor, iremos aprender um pouco sobe endereço de memória um pouco a frente.
+	
+	printf("Digite um número para a variável global: ");
+	
+	scanf("%d", &global); // Gravamos um valor na variável global.
+	
+	printf("\nO número na função principal é: %d\n", global);
+	
+	mostrarGlobal(); // vamos mostrar o valor na função a parte (está acima do main, apartir daqui, todas serão feitas assim).
+	
+	system("pause");
+	
+	system("cls");
+	
+	
+	
+	
 	
 		
 	puts("\n"); // A função puts significa "put string", que nada mais é que printar algo na tela, porém não recebe variaveis como o printf, apenas strings. 
@@ -334,12 +397,9 @@ int main(void){ // Chamando a função principal, onde o programa começa. Usamos o
 
 
 void printMsg(){ // Para fazermos a função, devemos primeiramente escrever o tipo de retorno. Como um procedimento é uma função sem retorno, escrevemos void (nada). Lembrando que a função está declarada antes do main. Leia as anotações da declaração lá em cima.
-	
-	
+		
 	printf("O Guigui é legal\n");		// Digitamos o código a ser executado aqui.		
-	
-	
-	
+		
 }
 
 
